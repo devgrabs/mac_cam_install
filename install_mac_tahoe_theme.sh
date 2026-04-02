a6f0225abc21d559ae6e4ebc00a0d089dd72edc9
devgrabs <roem21@gmail.com>
2026-04-02 19:12:59 +0900

    delete gnome extenstion

--- install_mac_tahoe_theme.sh
+++ install_mac_tahoe_theme.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 sudo apt install gnome-tweaks
-sudo apt install gnome-shell-extensions
 git clone https://github.com/vinceliuice/MacTahoe-gtk-theme.git --depth=1
 cd MacTahoe-gtk-theme
 sudo ./install.sh
